5a8b2cd39892ea81027eb6e5c5aff03aeaacc539
Rahul <rahulraveendran@accubits.com>
2018-12-03 02:12:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let first = 'rahul';
+let last = 'raveendran';
+let id = 2;
+
+let name = `Your name is ${first} ${last}.`
+let url = `http://localhost:3000/api/messages/${id}`
+
+console.log(name);
+console.log(url);
